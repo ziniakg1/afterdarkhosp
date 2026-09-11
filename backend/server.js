1fad029ca17b7b3042fc5867a67344990e64ad64
ziniakg1 <ziniakg1@southernct.edu>
2026-09-10 20:37:57 -0400

    Fix gmail

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ const upload = multer({
 });
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
