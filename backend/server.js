6dff3c9d79a03ed4b3d62018dd8b58b69901d919
ziniakg1 <ziniakg1@southernct.edu>
2026-09-10 20:27:52 -0400

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const app = express();
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
 const upload = multer({
     dest: "uploads/"
 });
@@ -92,6 +96,6 @@ Phone: ${phone}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
 });
